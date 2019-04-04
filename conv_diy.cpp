c656dade7bfdf005889f38c9018dd97f911e5ebb
socmgr <bonobonos>
2019-04-04 14:26:40 +0900

    update

--- conv_diy.cpp
+++ conv_diy.cpp
@@ -15,20 +15,25 @@ int your_conv( cv::Mat src,
     int kernel_height = kernel.rows;
     int kernel_width = kernel.cols;
 
-   // int dst_height;
-   //int dst_width;
+
+   dst = (cv::Mat_<float>(src_height-stride+1,src_width,3));
+
+
+
+  //  int dst_height=dst.rows;
+//    int dst_width=dst.cols;
 
     // src.ptr<unsigned char>(i)[ calculate INDEX ]
 
     // MAKE YOUR OWN CONVOLUTION PROCESS
 
 for(int a = 0; a< 3; a++){
- for(int k = 0; k< src_height; k=k+stride){
-  for(int r = 0; r< src_width; r=r+stride){
-   for (int i = 0; i<kernel_height; i++){
-    for (int j = 0; j< kernel_width; j++){
-       
-      dst.at<cv::Vec3b>(k,r)[a]+=kernel.at<float>(i,j)*src.at<cv::Vec3b>(i,j)[a];
+ for(int k = 0; k< src_width; k++){ 
+   for(int r = 0; r< src_height; r=r+stride){
+     for (int j = 0; j< kernel_width; j++){
+       for (int i = 0; i<kernel_height; i++){
+     
+     dst.at<cv::Vec3b>(r,k)[a]+=kernel.at<cv::Vec3b>(i,j)[a]*src.at<cv::Vec3b>(i,j)[a];
 
        }
         }
@@ -45,7 +50,7 @@ for(int a = 0; a< 3; a++){
 
 int main ( int argc, char** argv )
 {
-    if (argc < 2) {
+    if (argc < 1) {
         std::cout << "no filename given." << std::endl;
         std::cout << "usage: " << argv[0] << " image" << std::endl;
         return -1;
@@ -58,7 +63,9 @@ int main ( int argc, char** argv )
     if( !src.data )  { return -1; }
 
     // Make filter
-    kernel = (cv::Mat_<float>(3, 3) << -1, -1, -1, -1, 8, -1, -1, -1, -1);
+    kernel = (cv::Mat_<float>(3,3,3) << -1, -1, -1, -1, 8, -1, -1, -1, -1,
+                                         0,  0,  0,  0, 0,  0,  0,  0,  0, 
+                                         0,  0,  0,  0, 0,  0,  0,  0,  0);
 
 
     // Run 2D filter
